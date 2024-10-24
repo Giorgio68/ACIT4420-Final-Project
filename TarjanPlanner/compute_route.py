0f5d84f2f4744174bb03998ecb8ba6073eff328f
Giorgio Salvemini <giorgio@salvemini.no>
2024-10-24 11:27:07 +0200

    Updated mode of transport to singular json

---
+++ TarjanPlanner/compute_route.py
@@ -0,0 +1,19 @@
+"""
+Module used to compute and display the most efficient route between relatives
+"""
+
+import json
+from pathlib import Path
+from typing import Optional
+
+
+_MODES_OF_TRANSPORT: list[str | float] = None
+
+def _load_modes(f_name: Optional[Path] = None) -> None:
+    if f_name is None:
+        f_name = Path(".") / "data" / "mode_of_transport.json"
+
+    with open(f_name, "r", encoding="utf-8") as f_json:
+        _MODES_OF_TRANSPORT.append(json.loads(f_json))
+
+_load_modes()
