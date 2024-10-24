d81edd51e53150793c5d1380bfbe4462b1e6530d
Giorgio Salvemini <giorgio@salvemini.no>
2024-10-24 11:49:28 +0200

    Modified file

--- TarjanPlanner/compute_route.py
+++ TarjanPlanner/compute_route.py
@@ -7,14 +7,16 @@ from pathlib import Path
 from typing import Optional
 
 
-_MODES_OF_TRANSPORT: dict[str | float] = None
+_modes_of_transport: dict[str | float] = None
 
 def _load_modes(f_name: Optional[Path] = None) -> None:
+    global _modes_of_transport
+    
     if f_name is None:
         f_name = Path(".") / "data" / "mode_of_transport.json"
 
     with open(f_name, "r", encoding="utf-8") as f_json:
-        _MODES_OF_TRANSPORT.append(json.loads(f_json))
+        _modes_of_transport = json.loads(f_json)
 
 
 _load_modes()
