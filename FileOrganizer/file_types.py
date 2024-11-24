5ffbcc47b8701f585d8f4050c3645b509e00acb0
Giorgio Salvemini <giorgio@salvemini.no>
2024-11-24 22:28:34 +0100

    Formatting

--- FileOrganizer/file_types.py
+++ FileOrganizer/file_types.py
@@ -4,10 +4,10 @@ Contains all available file types that can be processed
 
 
 types = {
-        "python": {"extension": ["py", "pyc"]},
-        "text": {"extension": ["txt"]},
-        "json": {"extension": ["json", "jsonl"]},
-        "executables": {"extension": ["exe"]},
-        "image": {"extension": ["png", "jpg", "svg", "tiff", "webp"]},
-        "video": {"extension": ["mp4"]},
+    "python": {"extension": ["py", "pyc"]},
+    "text": {"extension": ["txt"]},
+    "json": {"extension": ["json", "jsonl"]},
+    "executables": {"extension": ["exe"]},
+    "image": {"extension": ["png", "jpg", "svg", "tiff", "webp"]},
+    "video": {"extension": ["mp4"]},
 }
