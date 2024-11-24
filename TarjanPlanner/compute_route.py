295139d33e2a1708047e6812b2959fb41fa63e47
Giorgio Salvemini <giorgio@salvemini.no>
2024-11-24 17:11:53 +0100

    Added regex checks when importing transport modes

--- TarjanPlanner/compute_route.py
+++ TarjanPlanner/compute_route.py
@@ -2,10 +2,12 @@
 Module used to compute and display the most efficient route between relatives
 """
 
+import re
 import json
 from pathlib import Path
 from typing import Optional
 from .exceptions import ModesImportFailed, RouteCalculationError, RouteDisplayingError
+from .decorators import log_func_call, time_func_call, cache
 
 
 _modes_of_transport: dict[str | float] = None
@@ -21,16 +23,30 @@ def _load_modes(f_name: Optional[Path] = None) -> None:
         with open(f_name, "rb") as f_json:
             _modes_of_transport = json.loads(f_json)
 
-    except FileNotFoundError as e:
+        for mode in _modes_of_transport:
+            if not re.match(r"[+-]?([0-9]*[.])?[0-9]+", mode["speed"]):
+                raise ModesImportFailed('param "speed" is not a number')
+
+            if not re.match(r"[+-]?([0-9]*[.])?[0-9]+", mode["cost_per_km"]):
+                raise ModesImportFailed('param "cost_per_km" is not a number')
+
+            if not re.match(r"[+-]?([0-9]*[.])?[0-9]+", mode["transfer_time_min"]):
+                raise ModesImportFailed('param "transfer_time_min" is not a number')
+
+    except (FileNotFoundError, ModesImportFailed) as e:
         raise ModesImportFailed("Failed to import modes of transport") from e
 
 
 _load_modes()
 
 
+@log_func_call
+@time_func_call
+@cache
 def calculate_route():
     pass
 
 
+@log_func_call
 def display_route():
     pass
